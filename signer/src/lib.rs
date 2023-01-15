d10219b968ee0b5db452e03e6678ba63a302a8ab
yayanyang <yayanyang@gmail.com>
2023-01-15 23:35:48 +0800

    update

--- signer/src/lib.rs
+++ signer/src/lib.rs
@@ -1,5 +1,12 @@
+use std::fmt::{Debug, Display};
+
+use ethers_utils_rs::types::{Bytecode, Transaction};
+use jsonrpc_rs::RPCResult;
+
 pub mod error;
 pub mod signers;
+mod typed;
+pub use typed::*;
 
 /// Signer `Result` type alias with [`error::SignerError`]
 pub type Result<T> = std::result::Result<T, error::SignerError>;
@@ -13,5 +20,20 @@ pub struct Signer {
 impl Signer {
     pub fn new(rpc_client: jsonrpc_rs::Client) -> Self {
         Self { rpc_client }
+    }
+
+    /// Returns the signed transaction of the parameter `transaction_request`
+    pub async fn sign_eth_transaction<T>(&mut self, transaction_request: T) -> RPCResult<Bytecode>
+    where
+        T: TryInto<Transaction>,
+        T::Error: Display + Debug,
+    {
+        let transaction_request = transaction_request
+            .try_into()
+            .map_err(jsonrpc_rs::map_error)?;
+
+        self.rpc_client
+            .call("signer_signEthTransaction", vec![transaction_request])
+            .await
     }
 }
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -4,3 +4,5 @@ pub mod error;
 pub mod hex;
 
 pub mod types;
+
+pub mod typed_data;
